bb575cc23dde666018558e40059f755cdff7ea03
luispmaraujo <luispmaraujo@outlook.com>
2021-05-16 17:49:40 -0300

    Criação dos arquivos da aula 'JSON'.

---
+++ json/js/index.js
@@ -0,0 +1,19 @@
+let obj = {
+  "lista de produtos": [
+    {
+      "codigo": 1,
+      "nome": "Torradeira",
+      "preco": 134.5,
+    },
+    {
+      "codigo": 2,
+      "nome": "Sanduicheira",
+      "preco": 120.3,
+    },
+    {
+      "codigo": 3,
+      "nome": "Refrigerador",
+      "preco": 1240,
+    },
+  ],
+};
