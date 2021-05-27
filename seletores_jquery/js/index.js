38ff5a1ba1c6e37be32194099136cc690382c568
luispmaraujo <luispmaraujo@outlook.com>
2021-05-27 14:24:09 -0300

    Criaçao dos arquivos da aula de seletores jQuery.

---
+++ seletores_jquery/js/index.js
@@ -0,0 +1,20 @@
+// $(document).ready(function () {
+//     $("#imagem").click(function (evento) {
+//       console.log("Olá Mundo!");
+//     });
+//   });
+
+// $(document).ready(function () {
+//   $("#imagem").click(function (evento) {
+//     console.log("Olá Mundo!");
+//   });
+// });
+
+$(document).ready(function () {
+  $("h1").css("background-color", "blue");
+  $("#p3").css("background-color", "green");
+  $(".primeiro").css("background-color", "red");
+  $("#btn, ul").css("background-color", "purple");
+  $("p:even").css("background-color", "gold");
+  $(':lang("en")').css("font-style", "italic"); //   $('[lang="en"]').css("font-style", "italic");
+});
